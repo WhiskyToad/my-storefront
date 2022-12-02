d7640e8039de61eb8ac4d813881a64c3e71df6af
Steven Craig <steven.craig@thrivelearning.com>
2022-12-02 14:56:43 +0000

    refactor: changed some names to lartsy and edited hero on homepage

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -6,13 +6,11 @@ const Hero = () => {
     <div className="h-[90vh] w-full relative">
       <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
         <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          Summer styles are finally here
+          Find your new favourite today
         </h1>
         <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-          This year, our new summer collection will shelter you from the harsh
-          elements of a world that doesn&apos;t care if you live or die.
+        Find your friendly neighborhood grocery. Food shopping has never been this fun! Local treasures await you.
         </p>
-        <UnderlineLink href="/store">Explore products</UnderlineLink>
       </div>
       <Image
         src="/hero.jpg"
